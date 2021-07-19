4d9403cc72830c8e09b5baeee20a540a55e79338
Bohdan-Kuzil <87597136+Bohdan-Kuzil@users.noreply.github.com>
2021-07-19 14:11:15 +0300

    Js_DZ1

---
+++ Lesson1/script.js
@@ -0,0 +1,24 @@
+
+let city
+city = 'Київ'
+city = 'Львів'
+console.log(city);
+// Створюємо змінну address і копіюємо в неї значення city
+let address = 'Львів'
+alert(address)
+
+
+// // 2.Завдання
+let n=3
+let result=Math.pow(n ,3)
+console.log(result);
+
+
+// 3.Завдання
+let pen=4
+let price_Pen=5.25
+let pencils=6
+let price_Pencils=3.50
+let result2=(pen * price_Pen)+(pencils * price_Pencils)
+document.write(result2)
+// end

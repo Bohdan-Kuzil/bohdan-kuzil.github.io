901dd4c4ee08281ac80bc3bcc97b439c7c7c890b
Bohdan-Kuzil <87597136+Bohdan-Kuzil@users.noreply.github.com>
2021-07-23 13:36:21 +0300

    Lesson_2_DZ

---
+++ Lesson2_Dz/season/script.js
@@ -0,0 +1,85 @@
+let ask = prompt('Напишіть число від 1 до 12')
+if(ask==1){
+    alert('Січень');
+}
+else if (ask == 2) {
+    alert('Лютий');
+}
+else if  (ask == 3) {
+    alert('Березень');
+}
+else if  (ask == 4) {
+    alert('Квітень');
+}
+else if  (ask == 5) {
+    alert('Травень');
+}
+else if  (ask == 6) {
+    alert('Червень');
+}
+else if  (ask == 7) {
+    alert('Липень');
+}
+else if  (ask == 8) {
+    alert('Серпень');
+}
+else if  (ask == 9) {
+    alert('Вересень');
+}
+else if  (ask == 10) {
+    alert('Жовтень');
+}
+else if  (ask == 11) {
+    alert('Листопад');
+}
+else if  (ask == 12) {
+    alert('Грудень');
+}
+else  {
+    alert('Неможливо');
+}
+    
+        //   Це лишаю тут як другий варіант відповіді)
+// let month = prompt("Напишіть число від 1 до 12");
+// switch (month) {
+//     case "1":
+//         alert("Січень");
+//         break;
+//     case "2":
+//         alert("Лютий");
+//         break;
+//     case "3":
+//         alert("Березень");
+//         break;
+//     case "4":
+//         alert("Квітень");
+//         break;
+//     case "5":
+//         alert("Травень");
+//         break;
+//     case "6":
+//         alert("Червень");
+//         break;
+//     case "7":
+//         alert("Липень");
+//         break;
+//     case "8":
+//         alert("Серпень");
+//         break;
+//     case "9":
+//         alert("Вересень");
+//         break;
+//     case "10":
+//         alert("Жовтень");
+//         break;
+//     case "11":
+//         alert("Листопад");
+//         break;
+//     case "12":
+//         alert("Грудень");
+//         break;
+
+//     default:
+//         alert("Неможливо")
+//         break;
+// }
